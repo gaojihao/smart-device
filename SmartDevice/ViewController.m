5536a16850606923f7edef90bfbfd28f417fac8c
lizhi <lizhi1026@126.com>
2019-12-15 23:13:14 +0800

    add project

--- SmartDevice/ViewController.m
+++ SmartDevice/ViewController.m
@@ -14,8 +14,10 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor redColor];
     // Do any additional setup after loading the view.
 }
 
